e02d5d6906a35aa9b2617a7395f83e5d53440ff4
bjoerm <bjoern-menke@web.de>
2022-08-07 17:08:21 +0200

    Bugfix

--- gpx_to_map.py
+++ gpx_to_map.py
@@ -4,12 +4,12 @@ from gpxplotter import add_segment_to_map, create_folium_map, read_gpx_file
 
 files = glob.glob("input/*.gpx")
 
+# Append converted .fit files
 former_fit_files = glob.glob("input/fit_to_gpx/*.gpx")
-
-files.append(former_fit_files)
+files.extend(former_fit_files)
 
 # Using a subset for tests.
-# files = files[0:15]
+# files = files[0:55]
 
 the_map = create_folium_map()
 
@@ -17,11 +17,11 @@ for file in files:
     print(file)
     for track in read_gpx_file(file):
         for i, segment in enumerate(track["segments"]):
-            add_segment_to_map(the_map=the_map, segment=segment, add_start_end=False, line_options={"opacity": 0.5, "weight": 5})
+            add_segment_to_map(the_map=the_map, segment=segment, add_start_end=False, line_options={"opacity": 0.10, "weight": 5})
 
 
 # To store the map as a HTML page:
-the_map.save("map_005.html")
+the_map.save("map_002.html")
 
 
 # To display the map in a Jupyter notebook:
